6e64e00ae05455b20d53f72e2c9fc921ab6b4408
mrichter-git <82392631+mrichter-git@users.noreply.github.com>
2021-04-22 08:53:12 +0200

    Changed Tof.c added Thread

--- ToF.c
+++ ToF.c
@@ -7,7 +7,40 @@
 #include "ToF.h"
 #include "VL53L0X.h"
 
-static uint16_t distance_mm = 0;
+static uint16_t dist_mm = 0;
+
+static THD_WORKING_AREA(waToFThd, 512);
+static THD_FUNCTION(ToFThd, arg) {
+
+	chRegSetThreadName("ToF Thd");
+	VL53L0X_Error status = VL53L0X_ERROR_NONE;
+
+	(void)arg;
+	static VL53L0X_Dev_t device;
+
+	device.I2cDevAddr = VL53L0X_ADDR;
+
+	status = VL53L0X_init(&device);
+
+	if(status == VL53L0X_ERROR_NONE){
+		VL53L0X_configAccuracy(&device, VL53L0X_DEFAULT_MODE);
+	}
+	if(status == VL53L0X_ERROR_NONE){
+		VL53L0X_startMeasure(&device, VL53L0X_DEVICEMODE_CONTINUOUS_RANGING);
+	}
+	if(status == VL53L0X_ERROR_NONE){
+		VL53L0X_configured = true;
+	}
+
+    /* Reader thread loop.*/
+    while (chThdShouldTerminateX() == false) {
+    	if(VL53L0X_configured){
+    		VL53L0X_getLastMeasure(&device);
+   			dist_mm = device.Data.LastRangeMeasure.RangeMilliMeter;
+    	}
+		chThdSleepMilliseconds(100);
+    }
+}
 
 void init_ToF(void) {
 
